df37c72997d4904f2a9d28917f0efa377533f406
Lívia Borges Ferreira <liviabf02@gmail.com>
2025-03-20 18:16:27 -0400

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,8 @@
+
+num=int(input("inserir numero"))
+if num % 2 == 0:
+    print=input("Esse numero é par")
+else:
+    print=input("Esse número é impar")
+
+
---
+++ index.py
@@ -0,0 +1,59 @@
+# isso é um comentário 
+# # print("Mensagem")
+# idade=6
+# altura=0.50
+# peso=5.950
+# nome="Francisco"
+# print(idade,altura,peso,nome)
+
+#nome=input("insira seu nome") #input texto=string
+# idade=int(input("insira sua idade")) #int numero inteiro
+# peso=float(input("insira seu peso")) #numero real
+# print(f"{nome}{idade}anos{peso}kg")  #mostrar para o usuário
+
+# pedaço_de_pizza=5 #declaração de variável
+# cliente="jonh"
+# print(type(pedaço_de_pizza))
+# print(type(cliente))
+
+# pedaço_de_pizza=input("Informe quantos pedaços comeu") #type mostra o tipo da variavél
+# print(type(pedaço_de_pizza)) #print mostra ao usuário
+# print=input(f"voce comeu" {pedaço_de_pizza}) #input sempre usado antes de um parentese com um texto
+
+# a=4
+# A="Salty"
+# print(a)
+# print(A)
+
+# frutal, fruta2, fruta3= "Laranja", "Banana", "Maça"
+# print(frutal, fruta2, fruta3)
+
+# primeiro_numero=5
+# segundo_numero=10
+# print(primeiro_numero+segundo_numero)
+
+# nome=input("Qual o vingador mais forte?")
+# print(f"Olá, {nome} !")
+# if nome == "huk":
+#     print ("Bem vindo vingador mais forte")
+# else:
+#     print("Acesso negado!")
+
+
+# x=5
+# if x>2 and x<10: # AND Ambas as consições devem ser verdadeiras 
+#     print("O numero está entre 2 e 10") 
+
+#x=5 
+# if x>2 or x<4: # A função do OR é retomar TRUE apenas uma condição precisa ser verdadeira 
+#     print("O numero é menor que 2 ou maior que 4") 
+
+x=5
+if not x>10: #Inverte a condição (x não é maior que 10)
+    print("o numero não é maior que 10") 
+
+    
+    if x> 2 and (y > 10 or not x == 5):
+        print("Condição complexa atendida")
+    else:
+        print("Condição nao atendida")
